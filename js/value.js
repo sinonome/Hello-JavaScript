7f15c7bc244b2eb777587f65566b603a7e6e3d45
pisce <pis.ceyatta@gmail.com>
2020-08-24 18:58:51 +0900

    first commit!

---
+++ js/value.js
@@ -0,0 +1,7 @@
+// 式の評価結果の例（コンソールには表示されない）
+1; // => 1
+const total = 42 + 42;
+// 変数の評価結果の例（コンソールには表示されない）
+total; // => 84
+// Console APIでコンソールに表示する例
+console.log("JavaScript"); // => "JavaScript"
---
+++ js/var.js
@@ -0,0 +1,6 @@
+// 値の上書きはできない
+const hoge = "Hoge";
+// 基本的に変数定義は let で行う
+let huga = "Huga";
+// var は使わない
+console.log(hoge + huga);
